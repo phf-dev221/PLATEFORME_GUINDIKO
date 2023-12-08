87b0ef3e4f2484b8635cc938c35cefe644b73f22
benbasse <moussabasse221@gmail.com>
2023-12-08 07:11:32 +0000

    Le request qui permet de faire les validations des événements lors des insertions

--- app/Http/Requests/CreateEvenementRequest.php
+++ app/Http/Requests/CreateEvenementRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CreateEvenementRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class CreateEvenementRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,37 @@ class CreateEvenementRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            "libelle" => "required",
+            "description" => "required",
+            "image" => "required|image|mimes:jpeg,png,jpg,gif|max:2048",
+            "date_evenement" => "required|date",
+            "heure_evenement" => "required",
+            "lieu" => "required"
+
+        ];
+    }
+
+        public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException (response()->json([
+            'success'=> false,
+            'error'=> true,
+            'message'=> 'erreur de validation',
+            'errorLists' => $validator->errors(),
+        ]));
+    }
+
+    public function messages()
+    {
+        return[
+            "libelle.required" => "le libelle doit être fourni",
+            "description" => "la description doit être fourni",
+            "image.required" => "l'image ne doit pas être null",
+            "image.image" => "Seul les images sont autorisés",
+            "date_evenement.required" => "La date est obligatoire",
+            "date_evenement.date" => "Le format date n'est pas correct",
+            "heure_evenement.required"=>"l'heure de l'événement est réquis",
+            "lieu.required" => "Le lieu est obligatoire"
         ];
     }
 }
