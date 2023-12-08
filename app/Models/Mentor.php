f303334cb6ce6afd41129853dcb301f5d4ba17ba
Hamady <hamadhyfall21@gmail.com>
2023-12-08 15:34:57 +0000

    logout

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Mentor extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
